98adabd5c89267fa623ba650f4bae8130a88acd1
jerry <309032663@qq.com>
2023-01-16 18:35:13 +0800

    update

---
+++ sort/bubbleSort/bubbleSort_android_kotlin/app/src/main/java/cn/lblbc/bubblesort/LblbcActivity.kt
@@ -0,0 +1,42 @@
+package cn.lblbc.bubblesort
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * 厦门大学计算机专业 | 前华为工程师
+ * 专注《零基础学编程系列》  http://lblbc.cn/blog
+ * 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
+ * 公众号：蓝不蓝编程
+ */
+class LblbcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lblbc)
+
+        val array = intArrayOf(2, 1, 5, 4, 3)
+        printArray(array)
+        sort(array)
+        printArray(array)
+    }
+
+    private fun sort(array: IntArray) {
+        for (i in 0 until array.size - 1) {
+            for (j in 0 until array.size - 1 - i) {
+                if (array[j] > array[j + 1]) {
+                    val tmp = array[j + 1]
+                    array[j + 1] = array[j]
+                    array[j] = tmp
+                }
+            }
+        }
+    }
+
+    private fun printArray(array: IntArray) {
+        Log.i("lanbulan", "--------------")
+        for (i in array.indices) {
+            Log.i("lanbulan", array[i].toString() + "")
+        }
+    }
+}
