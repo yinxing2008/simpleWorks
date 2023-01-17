6b025049d4cd4731e079bcb226c345f91b5fd88c
zhangzm <zhangzm@tz.mail.wo.cn>
2023-01-17 20:34:15 +0800

    udpate

--- sort/bubbleSort/bubbleSort_C#/MainWindow.xaml.cs
+++ sort/bubbleSort/bubbleSort_C#/MainWindow.xaml.cs
@@ -20,9 +20,54 @@ namespace BubbleSort
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int[] array = { 2, 1, 5, 4, 3 };
         public MainWindow()
         {
             InitializeComponent();
+            ShowArray(array);
         }
+
+        private void ShuffleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Shuffle();
+            ShowArray(array);
+        }
+        private void SortButton_Click(object sender, RoutedEventArgs e)
+        {
+            Sort(array);
+            ShowArray(array);
+        }
+        private void Shuffle()
+        {
+            array = new int[] { 2, 1, 5, 4, 3 };
+        }
+
+        private void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int tmp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = tmp;
+                    }
+                }
+            }
+        }
+
+        private void ShowArray(int[] array)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sb.Append(array[i]+ " ");
+            }
+            label.Content = sb.ToString();
+        }
+
     }
 }
