79f74fba7c411e1fa39778013065f7dc8d4e7c87
jerry <309032663@qq.com>
2023-01-19 07:11:49 +0800

    update

--- sort/quickSort/sort_C++/Lblbc.cpp
+++ sort/quickSort/sort_C++/Lblbc.cpp
@@ -2,35 +2,57 @@
 /// 专注《零基础学编程系列》  http://lblbc.cn/blog
 /// 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
 /// 公众号：蓝不蓝编程
-
+/// 
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void sort(int* array, int count)
+
+void print(const vector<int>& array)
 {
-	for (int i = 0; i < count - 1; i++)
-	{
-		for (int j = 0; j < count - 1 - i; j++)
-		{
-			if (array[j + 1] < array[j])
-			{
-				int tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-			}
+	for (auto val : array)
+		cout << val << " ";
+	cout << endl;
+}
+
+int partition(vector<int>& array, int low, int high)
+{
+	int pivot_value = array[low];//备份枢轴值
+	while (low < high) {
+		while (low < high && array[high] >= pivot_value) {
+			--high;
 		}
+		array[low] = array[high];
+		while (low < high && array[low] <= pivot_value) {
+			++low;
+		}
+		array[high] = array[low];
 	}
+	array[low] = pivot_value;
+
+	return low;
+}
+void quickSort(vector<int>& array, int low, int high)
+{
+	int pivot_index;
+	if (low < high)
+	{
+		pivot_index = partition(array, low, high);
+		quickSort(array, low, pivot_index - 1);
+		quickSort(array, pivot_index + 1, high);
+	}
+}
+
+void sort(vector<int>& array)
+{
+	quickSort(array, 0, array.size() - 1);
 }
 
 int main()
 {
-	int array[5] = { 2, 1, 5, 4, 3 };
-	int count = sizeof(array) / sizeof(int);
-	sort(array, count);
-	for (int i = 0; i < count; i++)
-	{
-		cout << array[i] << " ";
-	}
+	vector<int> array{ 2, 1, 5, 4, 3 };
+	cout << "排序前: "; print(array);
+	sort(array);
+	cout << "排序后: "; print(array);
 	return 0;
 }
-
