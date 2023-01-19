b7f9883a0cd466608a6f7f4ee0a3954aa794dbb5
jerry <309032663@qq.com>
2023-01-20 07:23:29 +0800

    update

--- sort/selectionSort/sort_nodejs/app/route/lblbc.js
+++ sort/selectionSort/sort_nodejs/app/route/lblbc.js
@@ -17,46 +17,20 @@ router.get('sort', async (ctx) => {
 array = [2, 1, 5, 4, 3]
 
 function sort() {
-  sortMe(0, array.length - 1);
-  return convertToStr(array);
-}
-
-function sortMe(slow, fast) {
-  let base = array[slow];
-  array[slow] = 0;
-  let left = slow;
-  let right = fast;
-  while (left < right) {
-
-    if (array[left] === 0) {
-      if (array[right] < base) {
-        array[left] = array[right];
-        array[right] = 0;
-        left = left + 1;
-      }
-      else {
-        right = right - 1;
-      }
-    } else if (array[right] === 0) {
-      if (array[left] >= base) {
-        array[right] = array[left];
-        array[left] = 0;
-        right = right - 1;
-      }
-      else {
-        left = left + 1;
+  var len = array.length
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = i; j < len; j++) {
+      if (array[j] < array[i]) {
+        let tmp = array[i]
+        array[i] = array[j]
+        array[j] = tmp
       }
     }
   }
-  array[left] = base;
-  if ((left - 1) - slow > 0) {
-    sortMe(slow, left - 1);
-  }
-  if (fast - (right + 1) > 0) {
-    sortMe(right + 1, fast);
-  }
-  return
+
+  return convertToStr(array);
 }
+
 
 function convertToStr(array) {
   var result = ''
