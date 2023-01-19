09d6772cef595282e22fd418463b120a0ac6ec66
jerry <309032663@qq.com>
2023-01-19 20:55:14 +0800

    update

--- sort/quickSort/sort_php/app/controller/SortController.php
+++ sort/quickSort/sort_php/app/controller/SortController.php
@@ -3,21 +3,21 @@
 //专注《零基础学编程系列》  http://lblbc.cn/blog
 //包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
 //公众号：蓝不蓝编程
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace app\controller;
 
-use Exception;
-use think\facade\Request;
-
-class SortController {
-    public function sort() {
+class SortController
+{
+    public function sort()
+    {
         $array = [2, 1, 5, 4, 3];
         $result = $this->sortMe($array);
         return var_dump($result);
     }
 
-    public function sortMe($arr) {
+    public function sortMe($arr)
+    {
         $length = count($arr);
         if (!is_array($arr) || $length <= 1) {
             return $arr;
