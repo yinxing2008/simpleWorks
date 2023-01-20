d14c518ac603f35967c8d3ae5ddf5973ed00b772
jerry <309032663@qq.com>
2023-01-21 06:20:44 +0800

    update

---
+++ sort/bucketSort/sort_C++/Lblbc.cpp
@@ -0,0 +1,46 @@
+﻿/// 厦门大学计算机专业 | 前华为工程师
+/// 专注《零基础学编程系列》  http://lblbc.cn/blog
+/// 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
+/// 公众号：蓝不蓝编程
+
+#include <iostream>
+using namespace std;
+
+void printArray(int* array, int count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		cout << array[i] << " ";
+	}
+}
+
+void sort(int* array, int count)
+{
+	for (int i = 0; i < count - 1; i++)
+	{
+		for (int j = 0; j < count - 1 - i; j++)
+		{
+			if (array[j + 1] < array[j])
+			{
+				int tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int array[5] = { 2, 1, 5, 4, 3 };
+	int count = sizeof(array) / sizeof(int);
+	printf("排序前:");
+	printArray(array, count);
+
+	sort(array, count);
+
+	printf("排序后:");
+	printArray(array, count);
+	return 0;
+}
+
