54adf7a2086d222427bd595e0ae1f4fb537997c9
jerry <309032663@qq.com>
2023-01-21 21:42:48 +0800

    update

--- sort/mergeSort/sort_php/app/controller/SortController.php
+++ sort/mergeSort/sort_php/app/controller/SortController.php
@@ -12,15 +12,42 @@ class SortController
     public function sort()
     {
         $array = [2, 1, 5, 4, 3];
-        for ($i = 0; $i < count($array) - 1; $i++) {
-            for ($j = 0; $j < count($array) - 1 - $i; $j++) {
-                if ($array[$j] > $array[$j + 1]) {
-                    $tmp = $array[$j];
-                    $array[$j] = $array[$j + 1];
-                    $array[$j + 1] = $tmp;
-                }
-            }
-        }
+        $start = 0;
+        $end = count($array) - 1;
+        $this->sortMe($array, $start, $end);
         return var_dump($array);
     }
+    public function sortMe(&$arr, $start, $end)
+    {
+        if ($start < $end) {
+            $mid = floor(($start + $end) / 2);
+            $this->sortMe($arr, $start, $mid);
+            $this->sortMe($arr, $mid + 1, $end);
+            $this->mergeSort($arr, $start, $mid, $end);
+        }
+    }
+
+    public function mergeSort(&$arr, $start, $mid, $end)
+    {
+        $i = $start;
+        $j = $mid + 1;
+        $tmp = [];
+        while ($i <= $mid && $j <= $end) {
+            if ($arr[$i] <= $arr[$j]) {
+                $tmp[] = $arr[$i++];
+            } else {
+                $tmp[] = $arr[$j++];
+            }
+        }
+        while ($i <= $mid) {
+            $tmp[] = $arr[$i++];
+        }
+        while ($j <= $end) {
+            $tmp[] = $arr[$j++];
+        }
+        for ($k = 0; $k < count($tmp); $k++) {
+            $arr[$k + $start] = $tmp[$k];
+        }
+    }
+
 }
