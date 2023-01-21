30aa889120328c195186a59f54c426d0da8ef74c
jerry <309032663@qq.com>
2023-01-21 15:49:40 +0800

    update

---
+++ sort/shellSort/sort_python/lblbc.py
@@ -0,0 +1,30 @@
+"""
+厦门大学计算机专业 | 前华为工程师
+专注《零基础学编程系列》  http://lblbc.cn/blog
+包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
+公众号：蓝不蓝编程
+"""
+array = [2, 1, 5, 4, 3]
+
+
+def sort():
+    n = len(array)
+    gap = int(n / 2)
+    while gap > 0:
+        for i in range(gap, n):
+            temp = array[i]
+            j = i
+            while j >= gap and array[j - gap] > temp:
+                array[j] = array[j - gap]
+                j -= gap
+            array[j] = temp
+        gap = int(gap / 2)
+
+
+print("排序前:")
+print(array)
+
+sort()
+
+print("排序后:")
+print(array)
