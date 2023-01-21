1add0bb74e9d78492730f5bfc51c3cfc2e6affb6
jerry <309032663@qq.com>
2023-01-21 15:52:57 +0800

    update

---
+++ sort/shellSort/sort_swiftUI/Sort/SortApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortApp.swift
+//  Sort
+//
+//  Created by jerry on 2023/1/18.
+//
+
+import SwiftUI
+
+@main
+struct SortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Lblbc()
+        }
+    }
+}
